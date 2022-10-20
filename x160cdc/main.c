8425538328af7233a9a53d6f91691bb95fbdc697
Fausto Saporito <fausto.saporito@thyssenkrupp.com>
2022-10-20 19:17:41 +0200

    Initial Commit

---
+++ x160cdc/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  x160cdc
+//
+//  Created by Fausto Saporito on 20/10/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
